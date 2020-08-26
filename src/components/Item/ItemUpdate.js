0f362c9fe950b7f8bd60c3d47787a35c8565c25e
JenCastioni <jennifer.castioni@gmail.com>
2020-08-26 16:41:00 -0400

    Update & show Success & failure messages for all
    Co-authored by: Naiema naiema.d.gardner@gmail.com
    Co-authored by: Emily em.deadman@gmail.com
    Co-authored by: Mel Mel.Nalband@gmail.com

---
+++ src/components/Item/ItemUpdate.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import ItemForm from './ItemForm'
+
+// import the api's url
+import apiUrl from '../../apiConfig'
+import messages from '../AutoDismissAlert/messages'
+
+// Import axios so we can make HTTP requests
+import axios from 'axios'
+
+class ItemUpdate extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      item: {
+        category: '',
+        product: '',
+        price: ''
+      },
+      updated: false
+    }
+  }
+
+  handleChange = event => {
+    event.persist()
+
+    this.setState(prevState => {
+      const updatedField = { [event.target.name]: event.target.value }
+
+      const editedItem = Object.assign({}, prevState.item, updatedField)
+
+      console.log('updatedField is', updatedField)
+      console.log('editedItem is', editedItem)
+
+      return { item: editedItem }
+    })
+  }
+
+  handleSubmit = event => {
+    // prevent the page from refreshing
+    event.preventDefault()
+    const { msgAlert } = this.props
+    axios({
+      url: `${apiUrl}/items/${this.props.match.params.id}/update`,
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${this.props.user.token}`
+      },
+      data: { item: this.state.item }
+    })
+      // if we succesfully updated the book, set the `updated` state to `true` to cause a redirect
+      .then(res => this.setState({ updated: true }))
+      .then(() => msgAlert({
+        heading: 'Updated Item Successfully',
+        message: messages.updateItemSuccess,
+        variant: 'success'
+      }))
+      .catch(error => {
+        msgAlert({
+          heading: 'Updated Item Failed' + error.message,
+          message: messages.updateItemFailure,
+          variant: 'danger'
+        })
+      })
+  }
+
+  render () {
+    const { item, updated } = this.state
+    const { handleChange, handleSubmit } = this
+
+    // when the user hits submit to finish editing the book
+    if (updated) {
+      // redirect to the show page (route)
+      return <Redirect to={`/items/${this.props.match.params.id}`} />
+    }
+
+    return (
+      <div>
+        <ItemForm
+          item={item}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          cancelPath={`/items/${this.props.match.params.id}`}
+        />
+      </div>
+    )
+  }
+}
+
+export default ItemUpdate

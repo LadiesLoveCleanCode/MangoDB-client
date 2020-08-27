64bbb4f62e6a43d25d818ed4a8cc35035d0f84be
Emily Deadman <emilydeadman@Emilys-MacBook-Pro.local>
2020-08-27 11:08:14 -0400

    Styling updates: Changed blue nav bar to match background, added background image, font-family

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,9 +25,8 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar className="navbar" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      MangoDB-client
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">

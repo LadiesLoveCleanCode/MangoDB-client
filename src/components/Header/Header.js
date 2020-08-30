e210411386ce168eab9fc0e385c17c5f0358c887
Naiema Gardner <naiema@Naiemas-MacBook-Pro.local>
2020-08-30 15:41:49 -0400

    Edited home button URL Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link className="navlink" href="/#/">Home</Nav.Link>
+    <Nav.Link className="navlink" href="/MangoDB-client/#/">Home</Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
     <Nav.Link href="#items-create">Manage Your Inventory</Nav.Link>
